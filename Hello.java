85ce181973e9a38c45c8cfb818bc454d10890847
vivekyadav-cdac <vivekyadavcdac@gmail.com>
2025-04-23 18:02:02 +0530

    First Commit of branch aux

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 
 public class Hello {
 	public static void main (String []args) {
-		System.out.print("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("I am learning git");
 	}
 }
